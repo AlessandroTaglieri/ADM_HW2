c502c0b245b2b91e7786adce968c5574ab9d7626
yvesnana <56451020+yvesnana@users.noreply.github.com>
2019-10-23 19:46:21 +0200

    Update CRQ2_1.py

--- CRQ2_1.py
+++ CRQ2_1.py
@@ -57,10 +57,9 @@ def infos(atlet, event_name):
     pos = pd.DataFrame(eventt['positions']) # the column containing all the events we are interrested in
     
     for i in pos['positions']:         # [{'y': 2, 'x':3}, {'y': 20, 'x':3}] 
-            x_pos.append(i[0]['x'])     # I collect the x in the list X_pos and y in the list Y_pos
-            x_pos.append(i[1]['x'])
-            y_pos.append(i[0]['y'])
-            y_pos.append(i[1]['y'])
+            x_pos.append(i[0]['x']*1.04)     # I collect the x in the list X_pos and y in the list Y_pos
+            y_pos.append(i[0]['y']*0.68)
+
             
     return(x_pos, y_pos)
 
@@ -110,223 +109,122 @@ len(ronaldo_pos[0])/2 # To see the number of events Ronaldo partecipated in
 # 
 # 
 
-
-
 from matplotlib.patches import Arc  
-def draw_pitch(pitch, line, orientation,view):
+def draw_pitch(pitch, line):
     
-    orientation = orientation  # “v" for vertical, “h" for horizontal
-    view = view # 'full' or 'half' 
+ 
     line = line # color of the lines
     pitch = pitch # color of the pitch
     
     # setting the plot size and correct orientation
-    if orientation.lower().startswith("h"):
-        
-        if view.lower().startswith("h"):
-            fig,ax = plt.subplots(figsize=(6.8,10.4))
-            plt.xlim(49,105)
-            plt.ylim(-1,69)
-        else:
-            fig,ax = plt.subplots(figsize=(10.4,6.8))
-            plt.xlim(-1,105)
-            plt.ylim(-1,69)
-        ax.axis('off') # this hides the x and y ticks
-    
-        # side and goal lines #
-        ly1 = [0,0,68,68,0]
-        lx1 = [0,104,104,0,0]
 
-        plt.plot(lx1,ly1,color=line,zorder=5)
+    fig,ax = plt.subplots(figsize=(20,15))
+    plt.xlim(-1,105)
+    plt.ylim(-1,69)
+    ax.axis('off') # this hides the x and y ticks
+
+    # side and goal lines #
+    ly1 = [0,0,68,68,0]
+    lx1 = [0,104,104,0,0]
+
+    plt.plot(lx1,ly1,color=line,zorder=5)
 
 
-        # boxes, 6 yard box and goals
+    # boxes, 6 yard box and goals
 
-            #outer boxes#
-        ly2 = [13.84,13.84,54.16,54.16] 
-        lx2 = [104,87.5,87.5,104]
-        plt.plot(lx2,ly2,color=line,zorder=5)
+        #outer boxes#
+    ly2 = [13.84,13.84,54.16,54.16] 
+    lx2 = [104,87.5,87.5,104]
+    plt.plot(lx2,ly2,color=line,zorder=5)
 
-        ly3 = [13.84,13.84,54.16,54.16] 
-        lx3 = [0,16.5,16.5,0]
-        plt.plot(lx3,ly3,color=line,zorder=5)
+    ly3 = [13.84,13.84,54.16,54.16] 
+    lx3 = [0,16.5,16.5,0]
+    plt.plot(lx3,ly3,color=line,zorder=5)
 
-            #goals#
-        ly4 = [30.34,30.34,37.66,37.66]
-        lx4 = [104,104.2,104.2,104]
-        plt.plot(lx4,ly4,color=line,zorder=5)
+        #goals#
+    ly4 = [30.34,30.34,37.66,37.66]
+    lx4 = [104,104.2,104.2,104]
+    plt.plot(lx4,ly4,color=line,zorder=5)
 
-        ly5 = [30.34,30.34,37.66,37.66]
-        lx5 = [0,-0.2,-0.2,0]
-        plt.plot(lx5,ly5,color=line,zorder=5)
-
-
-           #6 yard boxes#
-        ly6 = [24.84,24.84,43.16,43.16]
-        lx6 = [104,99.5,99.5,104]
-        plt.plot(lx6,ly6,color=line,zorder=5)
-
-        ly7 = [24.84,24.84,43.16,43.16]
-        lx7 = [0,4.5,4.5,0]
-        plt.plot(lx7,ly7,color=line,zorder=5)
-
-        #Halfway line, penalty spots, and kickoff spot
-        ly8 = [0,68] 
-        lx8 = [52,52]
-        plt.plot(lx8,ly8,color=line,zorder=5)
+    ly5 = [30.34,30.34,37.66,37.66]
+    lx5 = [0,-0.2,-0.2,0]
+    plt.plot(lx5,ly5,color=line,zorder=5)
 
 
-        plt.scatter(93,34,color=line,zorder=5)
-        plt.scatter(11,34,color=line,zorder=5)
-        plt.scatter(52,34,color=line,zorder=5)
+       #6 yard boxes#
+    ly6 = [24.84,24.84,43.16,43.16]
+    lx6 = [104,99.5,99.5,104]
+    plt.plot(lx6,ly6,color=line,zorder=5)
 
-        # circle1 = plt.Circle((93.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        # circle2 = plt.Circle((10.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle3 = plt.Circle((52, 34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
-        
-        #Prepare Arcs
-        leftArc = Arc((93.9,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
-                       zorder=2,alpha=1,angle=0,theta1=130,theta2=230)
-        rightArc = Arc((10.1,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
-                       zorder=2,alpha=1,angle=180,theta1=130,theta2=230)
+    ly7 = [24.84,24.84,43.16,43.16]
+    lx7 = [0,4.5,4.5,0]
+    plt.plot(lx7,ly7,color=line,zorder=5)
 
-        #Draw Arcs
-        ax.add_patch(leftArc)
-        ax.add_patch(rightArc)
-
-        ## Rectangles in boxes
-        rec1 = plt.Rectangle((87.5,20), 16,30,ls='-',color=pitch, zorder=1,alpha=1)
-        rec2 = plt.Rectangle((0, 20), 16.5,30,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ## Pitch rectangle
-        rec3 = plt.Rectangle((-1, -1), 106,70,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ax.add_artist(rec3)
-        #Draw Arcs
-        ax.add_patch(leftArc)
-        ax.add_patch(rightArc)
-        ax.add_artist(rec1)
-        ax.add_artist(rec2)
-        ax.add_artist(circle3)
-        
-    else:
-        if view.lower().startswith("h"):
-            fig,ax = plt.subplots(figsize=(10.4,6.8))
-            plt.ylim(49,105)
-            plt.xlim(-1,69)
-        else:
-            fig,ax = plt.subplots(figsize=(6.8,10.4))
-            plt.ylim(-1,105)
-            plt.xlim(-1,69)
-        ax.axis('off') # this hides the x and y ticks
-
-        # side and goal lines #
-        lx1 = [0,0,68,68,0]
-        ly1 = [0,104,104,0,0]
-
-        plt.plot(lx1,ly1,color=line,zorder=5)
+    #Halfway line, penalty spots, and kickoff spot
+    ly8 = [0,68] 
+    lx8 = [52,52]
+    plt.plot(lx8,ly8,color=line,zorder=5)
 
 
-        # boxes, 6 yard box and goals
+    plt.scatter(93,34,color=line,zorder=5)
+    plt.scatter(11,34,color=line,zorder=5)
+    plt.scatter(52,34,color=line,zorder=5)
 
-            #outer boxes#
-        lx2 = [13.84,13.84,54.16,54.16] 
-        ly2 = [104,87.5,87.5,104]
-        plt.plot(lx2,ly2,color=line,zorder=5)
+    # circle1 = plt.Circle((93.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
+    # circle2 = plt.Circle((10.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
+    circle3 = plt.Circle((52, 34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
 
-        lx3 = [13.84,13.84,54.16,54.16] 
-        ly3 = [0,16.5,16.5,0]
-        plt.plot(lx3,ly3,color=line,zorder=5)
+    #Prepare Arcs
+    leftArc = Arc((93.9,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
+                   zorder=2,alpha=1,angle=0,theta1=130,theta2=230)
+    rightArc = Arc((10.1,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
+                   zorder=2,alpha=1,angle=180,theta1=130,theta2=230)
 
-            #goals#
-        lx4 = [30.34,30.34,37.66,37.66]
-        ly4 = [104,104.2,104.2,104]
-        plt.plot(lx4,ly4,color=line,zorder=5)
+    #Draw Arcs
+    ax.add_patch(leftArc)
+    ax.add_patch(rightArc)
 
-        lx5 = [30.34,30.34,37.66,37.66]
-        ly5 = [0,-0.2,-0.2,0]
-        plt.plot(lx5,ly5,color=line,zorder=5)
+    ## Rectangles in boxes
+    rec1 = plt.Rectangle((87.5,20), 16,30,ls='-',color=pitch, zorder=1,alpha=1)
+    rec2 = plt.Rectangle((0, 20), 16.5,30,ls='-',color=pitch, zorder=1,alpha=1)
 
+    ## Pitch rectangle
+    rec3 = plt.Rectangle((-1, -1), 106,70,ls='-',color=pitch, zorder=1,alpha=1)
 
-           #6 yard boxes#
-        lx6 = [24.84,24.84,43.16,43.16]
-        ly6 = [104,99.5,99.5,104]
-        plt.plot(lx6,ly6,color=line,zorder=5)
-
-        lx7 = [24.84,24.84,43.16,43.16]
-        ly7 = [0,4.5,4.5,0]
-        plt.plot(lx7,ly7,color=line,zorder=5)
-
-        #Halfway line, penalty spots, and kickoff spot
-        lx8 = [0,68] 
-        ly8 = [52,52]
-        plt.plot(lx8,ly8,color=line,zorder=5)
-
-
-        plt.scatter(34,93,color=line,zorder=5)
-        plt.scatter(34,11,color=line,zorder=5)
-        plt.scatter(34,52,color=line,zorder=5)
-
-        # circle1 = plt.Circle((93.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        # circle2 = plt.Circle((10.5,34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=1,alpha=1)
-        circle3 = plt.Circle((52, 34), 9.15,ls='solid',lw=1.5,color=line, fill=False, zorder=2,alpha=1)
-        
-        #Prepare Arcs
-        leftArc = Arc((93.9,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
-                       zorder=2,alpha=1,angle=0,theta1=130,theta2=230)
-        rightArc = Arc((10.1,34),height=18.30,ls='solid',width=25.3,color=line, fill=False, 
-                       zorder=2,alpha=1,angle=180,theta1=130,theta2=230)
-
-        
-
-
-        ## Rectangles in boxes
-        rec1 = plt.Rectangle((20, 87.5), 30,16.5,ls='-',color=pitch, zorder=1,alpha=1)
-        rec2 = plt.Rectangle((20, 0), 30,16.5,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ## Pitch rectangle
-        rec3 = plt.Rectangle((-1, -1), 70,106,ls='-',color=pitch, zorder=1,alpha=1)
-
-        ax.add_artist(rec3)
-        #Draw Arcs
-        ax.add_patch(leftArc)
-        ax.add_patch(rightArc)
-        ax.add_artist(rec1)
-        ax.add_artist(rec2)
-        ax.add_artist(circle3)
-        
+    ax.add_artist(rec3)
+    #Draw Arcs
+    ax.add_patch(leftArc)
+    ax.add_patch(rightArc)
+    ax.add_artist(rec1)
+    ax.add_artist(rec2)
+    ax.add_artist(circle3)
+    plt.gca().invert_yaxis()
 # draw_pitch("#195905","#faf0e6","h","full")
 
 # first plot
 
-draw_pitch("#195905","#faf0e6","horizontal","full")
+draw_pitch("#195905","#faf0e6")
 plt.scatter(messi_pos[0],messi_pos[1],marker='o',color='red',edgecolors="black", zorder=30)
-plt.gca().invert_yaxis()
 plt.title("Messi's positons - scatter plot")
 plt.show()
 
 # second plot
 
-draw_pitch("#195905","black","horizontal","full")
-sns.kdeplot(messi_pos[0],messi_pos[1], shade = "True", color = "green", n_levels = 30,zorder=2)
-
-plt.gca().invert_yaxis() # to invert respext to the y axis. This correspind to messi's real movements.
+draw_pitch("#195905","black")
+sns.kdeplot(messi_pos[0],messi_pos[1], shade = "True", color = "green", n_levels = 40,zorder=2)
 plt.title("Messi's more active zone - Heatmap")
 plt.show()
 
 # third plot
 
-draw_pitch("#195905","#faf0e6","horizontal","full")
+draw_pitch("#195905","#faf0e6")
 plt.scatter(ronaldo_pos[0],ronaldo_pos[1],marker='o',color='red',edgecolors="black", zorder=2)
-plt.gca().invert_yaxis()
 plt.title("Ronaldo's positons - scatter plot")
 plt.show()
 
 # fourth plot
-
-draw_pitch("#195905","black","horizontal","full")
-sns.kdeplot(ronaldo_pos[0],ronaldo_pos[1], shade = "True", color = "green", n_levels = 30,zorder=2)
-plt.gca().invert_yaxis()
+draw_pitch("#195905","black")
+sns.kdeplot(ronaldo_pos[0],ronaldo_pos[1], shade = "True", color = "green", n_levels = 40,zorder=2)
 plt.title("Ronaldo's more active zone - Heatmap")
 plt.show()
 
